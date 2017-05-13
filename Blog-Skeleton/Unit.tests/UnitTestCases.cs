27494d54fdb9832522f5d15e2727c7d48af3e83e
dimo <dimo_yanev@abv.bg>
2017-05-13 21:31:17 +0300

    Changes regarding UI tests

--- Blog-Skeleton/Unit.tests/UnitTestCases.cs
+++ Blog-Skeleton/Unit.tests/UnitTestCases.cs
@@ -5,19 +5,11 @@
     [TestFixture]
     public class UnitTestCases
     {
-        // 1. Navigate to Blog homepage
-        [Test, Property("Priority", 1)]
-        [Author("Author")]
-        public void LoadHomePage()
+        [Test]
+        public void TestMethod()
         {
-            IWebDriver driver = new ChromeDriver();
-            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(60));
-
-
-            driver.Navigate().GoToUrl(@"http://localhost:60634/Article/List");
-
-            var logo = wait.Until(w => w.FindElement(By.XPath("/html/body/div[1]/div/div[1]/a")));
-
-            Assert.AreEqual("SOFTUNI BLOG", logo.Text);
+            // TODO: Add your test code here
+            Assert.Pass("Your first passing test");
         }
+    }
 }
