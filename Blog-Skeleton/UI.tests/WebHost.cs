482738bff8969727691fada83ac0d3b305a35dae
Todorov-TDT <todorovtdt@gmail.com>
2017-05-20 17:52:07 +0300

    update webhost

--- Blog-Skeleton/UI.tests/WebHost.cs
+++ Blog-Skeleton/UI.tests/WebHost.cs
@@ -7,18 +7,10 @@
     {
         public static readonly SelenoHost Instance = new SelenoHost();
         public static readonly string RootUrl = @"http://localhost:60634/Article/List";
-       
 
         static WebHost()
         {
-<<<<<<< HEAD
-=======
-           
-            Instance.Run("Blog", 60634);
->>>>>>> 488381d794f49bc438cc0d07c5d2bad179cfc9ee
-
             Instance.Run("Blog", 60639, w => w.WithRemoteWebDriver(() => new ChromeDriver()));
-
         }
     }
 }
