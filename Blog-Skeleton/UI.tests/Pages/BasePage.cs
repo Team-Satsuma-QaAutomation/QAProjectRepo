c89031d52d7989db2cfed948bf65adb566036742
dimo <dimo_yanev@abv.bg>
2017-05-21 00:47:20 +0300

    Changes on driverwait

--- Blog-Skeleton/UI.tests/Pages/BasePage.cs
+++ Blog-Skeleton/UI.tests/Pages/BasePage.cs
@@ -14,7 +14,7 @@
         public BasePage(IWebDriver driver)
         {
             this.driver = driver;
-            this.wait = new WebDriverWait(this.driver, TimeSpan.FromSeconds(10));
+            this.wait = new WebDriverWait(this.driver, TimeSpan.FromSeconds(180));
         }
 
         public IWebDriver Driver
